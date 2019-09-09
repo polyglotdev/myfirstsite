bf4d5244e06226b60381b52bf05b97ebd03bf8a9
codefuldom <dom@codefuldom.com>
2019-09-08 20:28:21 -0500

    :bug: fixing bug in the home and about pages browser tab display to reflect about and home in tab

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,6 @@
 @extends('layout')
 
+@section('title', 'About')
 @section('content')
     <h1>About Us</h1>
     <p>Excepturi modi quisquam asperiores omnis eos placeat et. Exercitationem nisi aut vitae. Facilis et modi non. Est possimus ipsa dolore architecto officia hic sed temporibus. Nostrum aut sint impedit totam atque.</p>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @extends('layout')
 
+@section('title', 'CodefulDom')
 @section('content')
     <h1>My First Website</h1>
 @endsection
