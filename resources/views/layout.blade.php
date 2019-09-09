cb46622af172439799752f32db939283942065db
codefuldom <dom@codefuldom.com>
2019-09-08 20:10:43 -0500

    :fireworks: starting the blade templates module

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,13 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Contact Us</title>
+  <title></title>
 </head>
 <body>
-  <h1>Contact Us</h1>
   <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/about">Learn more about us</a></li>
+    <li><a href="/about">About Us</a></li>
+    <li><a href="/contact">Contact us to learn more</a></li>
   </ul>
+  @yield('content')
 </body>
 </html>
